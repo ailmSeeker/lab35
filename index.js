56c1cca3e5261d5155046c46888a54583a6017e6
Mohamed Yusuf <study@MohamedYusufs-MacBook-Pro.local>
2023-06-13 09:48:45 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 84;
+let num4 = 20;
+
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10); 
+let mod = num3 % num4;
+let max = Math.max(1,2,3,4,5,6,20,3);
